a276887d3fd6e810e4db04d16a4d5c1983d90155
satyam <satyamkale271@gmail.com>
2024-08-09 23:21:37 +0530

    class based state update

--- src/App.js
+++ src/App.js
@@ -60,14 +60,34 @@ import React from "react";
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { count: 0 };
+    this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
   }
+  handleDecrement() {
+    console.log(this);
+    this.setState((currState) => {
+      return {
+        count: currState.count - 1,
+      };
+    });
+  }
+
+  handleIncrement() {
+    console.log(this);
+    this.setState((currState) => {
+      return {
+        count: currState.count + 1,
+      };
+    });
+  }
+
   render() {
     return (
       <div>
-        <button>-</button>
-        <span>0</span>
-        <button>+</button>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
