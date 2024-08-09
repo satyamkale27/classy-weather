31780afa95f76e667f35e35e39887f7db422323f
satyam <satyamkale271@gmail.com>
2024-08-09 22:52:41 +0530

    class state

--- src/App.js
+++ src/App.js
@@ -58,6 +58,10 @@
 
 import React from "react";
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
   render() {
     return (
       <div>
