1036f349b5e751b32ee0d49362c57c5e820f7741
satyam <satyamkale271@gmail.com>
2024-08-10 22:17:53 +0530

    set state(location) in class approach

--- src/App.js
+++ src/App.js
@@ -58,13 +58,23 @@
 
 import React from "react";
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { location: "lisbon" };
+  }
   render() {
     return (
       <div className="app">
         <h1>Classy weather</h1>
         <div>
-          <input type="text" placeholder="search from location..." />
+          <input
+            type="text"
+            placeholder="search from location..."
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
         </div>
+        <button>Get weather</button>
       </div>
     );
   }
